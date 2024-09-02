2068817271e4bc47fce9c12b675a2b28702625df
PongsatornBoontham <161867621+PongsatornBT@users.noreply.github.com>
2024-09-02 12:47:18 +0700

    fixed bug wine-list-view

--- src/views/WineListView.vue
+++ src/views/WineListView.vue
@@ -170,7 +170,6 @@ export default {
         onMounted (() => {
             importCartId.value = route.params.cartId;
             token.value = localStorage.getItem('token')
-
             fetchCartItem()
         })
 
